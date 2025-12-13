1bfa9d3cbfc64ad43bbbd257534e9db2a42078ef
EL Mehdi Jessous <elmehdijessous01@gmail.com>
2025-12-14 00:54:40 +0100

    Add test user views !

--- coreApp/views.py
+++ coreApp/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
 
 # Create your views here.
 
 @login_required
 def home(request):
-    return render(request,"coreApp/home.html")
+    posts = Posts.objects.all()
+    form = FormPosts()
+    if request.method == "POST":
+        form = FormPosts(request.POST)
+        if form.is_valid():
+            y = form.save(commit=False)
+            y.user = request.user
+            y.save()
+            form.save_m2m()
+            return redirect("home")
+    context = {'form':form,'posts':posts}
+    return render(request,"coreApp/home.html",context)
